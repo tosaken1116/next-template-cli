fabc86eb2b52af5b9b02b16a1365d3f7491d25f7
tosaken1116 <nagato20021122@gmail.com>
2024-01-16 15:09:21 +0900

    feat: change template scripts

--- helper/addScripts.ts
+++ helper/addScripts.ts
@@ -2,35 +2,95 @@ import path from "path";
 import { GenerateConfigType } from "../generator";
 import { Tools } from "./getTemplate";
 import { readFileSync, writeFileSync } from "fs";
-
-const scripts: Record<Tools, Record<string, string>> = {
+type Scripts = Record<string, string>;
+const scripts: Record<Tools, Record<GenerateConfigType["size"], Scripts>> = {
     biome: {
-        lint: "biome lint --apply --no-errors-on-unmatched",
+        small: {
+            lint: "biome lint --apply --no-errors-on-unmatched",
+        },
+        medium: {
+            lint: "biome lint --apply --no-errors-on-unmatched",
+        },
+        large: {
+            lint: "biome lint --apply --no-errors-on-unmatched",
+        },
     },
     eslint: {
-        lint: "eslint 'src/**/*.{ts,tsx}' --report-unused-disable-directives --max-warnings 0",
+        small: {
+            lint: "eslint 'src/**/*.{ts,tsx}' --report-unused-disable-directives --max-warnings 0",
+        },
+        medium: {
+            lint: "eslint 'src/**/*.{ts,tsx}' --report-unused-disable-directives --max-warnings 0",
+        },
+        large: {
+            lint: "eslint 'src/**/*.{ts,tsx}' --report-unused-disable-directives --max-warnings 0",
+        },
     },
     hygen: {
-        "new:ui": "hygen generator ui",
-        "new:model": "hygen generator model",
-        "new:page": "hygen generator page",
-        "new:domain": "hygen generator domain",
+        small: {
+            new: "hygen generator component",
+        },
+        medium: {
+            "new:ui": "hygen generator ui",
+            "new:model": "hygen generator model",
+        },
+        large: {
+            "new:ui": "hygen generator ui",
+            "new:model": "hygen generator model",
+            "new:page": "hygen generator page",
+            "new:domain": "hygen generator domain",
+        },
     },
     jest: {
-        test: "jest",
+        small: {
+            test: "jest",
+        },
+        medium: {
+            test: "jest",
+        },
+        large: {
+            test: "jest",
+        },
     },
     vitest: {
-        test: "vitest",
+        small: {
+            test: "vitest",
+        },
+        medium: {
+            test: "vitest",
+        },
+        large: {
+            test: "vitest",
+        },
     },
     scaffdog: {
-        "new:ui": "scaffdog ui",
-        "new:model": "scaffdog model",
-        "new:page": "scaffdog page",
-        "new:domain": "scaffdog domain",
+        small: {},
+        medium: {
+            "new:ui": "scaffdog ui",
+            "new:model": "scaffdog model",
+            "new:page": "scaffdog page",
+            "new:domain": "scaffdog domain",
+        },
+        large: {
+            "new:ui": "scaffdog ui",
+            "new:model": "scaffdog model",
+            "new:page": "scaffdog page",
+            "new:domain": "scaffdog domain",
+        },
     },
     storybook: {
-        storybook: "start-storybook -p 6006",
-        "build-storybook": "storybook build",
+        small: {
+            storybook: "start-storybook -p 6006",
+            "build-storybook": "storybook build",
+        },
+        medium: {
+            storybook: "start-storybook -p 6006",
+            "build-storybook": "storybook build",
+        },
+        large: {
+            storybook: "start-storybook -p 6006",
+            "build-storybook": "storybook build",
+        },
     },
 };
 
@@ -48,9 +108,7 @@ export const addScripts = ({
     if (needStorybook) {
         tools.push("storybook");
     }
-    const addScripts = tools
-        .map((tool) => scripts[tool])
-        .reduce((acc, cur) => ({ ...acc, ...cur }), {});
+    const addScripts = tools.map((tool) => scripts[tool][size]);
 
     const packageJsonPath = path.join(projectRoot, "package.json");
     const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf8"));
