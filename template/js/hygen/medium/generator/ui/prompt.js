7b3cebaa0e61a49a04d38968181b6b5c0cae99e9
tosaken1116 <nagato20021122@gmail.com>
2024-01-19 18:34:23 +0900

    feat: support Pascal case in hygen

--- template/js/hygen/medium/generator/ui/prompt.js
+++ template/js/hygen/medium/generator/ui/prompt.js
@@ -1,14 +1,15 @@
 module.exports = {
-  prompt: ({ prompter }) => {
-    return prompter
-      .prompt([
-        {
-          type: 'input',
-          name: 'name',
-          message: 'component name',
-        },
-      ]).then(({ name }) => ({
-        name
-      }));
-  },
+    prompt: ({ prompter }) => {
+        return prompter
+            .prompt([
+                {
+                    type: "input",
+                    name: "name",
+                    message: "component name",
+                },
+            ])
+            .then(({ name }) => ({
+                name: name.slice(0, 1).toUpperCase() + name.slice(1),
+            }));
+    },
 };
